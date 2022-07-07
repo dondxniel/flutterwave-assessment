361a5fda99f5143b4cea47ff9b189a04458c5784
Daniel Don <dondaniel028@gmail.com>
2022-07-07 09:54:40 +0100

    Fixed port bug

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.post("/split-payments/compute", (req, res) => {
 	});
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server started on port 5000");
 });
